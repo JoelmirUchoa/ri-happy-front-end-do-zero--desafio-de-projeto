7358cf4638d85621ac46a33b102443c4de59db6a
JoelmirUchoa <149214348+JoelmirUchoa@users.noreply.github.com>
2024-12-11 17:04:48 -0300

    Erro corrigido

--- Jogo-Da-Memória-Emojis/src/scripts/engine.js
+++ Jogo-Da-Memória-Emojis/src/scripts/engine.js
@@ -29,7 +29,7 @@ for(let i = 0; i < emojis.length; i++){
 }
 
 function handleClick(){
-    if((openCards.length < 2){
+    if (openCards.length < 2){
         this.classList.add("boxOpen");
         openCards.push(this);
     }
